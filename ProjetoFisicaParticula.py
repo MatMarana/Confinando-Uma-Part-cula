3e7bc21c7e1e45497322317745d6e100c0bee9c7
mateus marana <assumateus@gmail.com>
2024-05-01 01:04:00 -0300

    features[funçãoOnda;energiaNivelQuantico; energiaFoton; conversor; foton; velocidadeParticula; comprimentoDeOndaDeBroglie ]

--- ProjetoFisicaParticula.py
+++ ProjetoFisicaParticula.py
@@ -3,8 +3,79 @@ from math import sqrt, sin
 ladoCaixa = float(input("Digite o largura da caixa"))
 nInicial = int(input("Digite o n Inicial"))
 nFinal = int(input("Digite o n Final"))
-massaParticula = 9.11 * (10 ** -31)
-PLANCK = 4.136 #E-15 em eV
+
+MASSAELETRON = 9.11e-31
+MASSAPROTON = 1.67E-27
+PLANCKEV = 4.136e-15
+PLANCKJS = 6.26e-34
+PI = 3.14
+LUZ = 3e8
+
+def funcaoOnda(ladoCaixa,n):
+    raiz = sqrt(2/ladoCaixa)
+    seno = (n * PI) / ladoCaixa
+    
+    return raiz,seno
+
+def energiaNivelQuantico(ladoCaixa, n, particula):
+    if particula == 'Eletron':
+        energia = ((PLANCKJS**2) / 8 * MASSAELETRON * (ladoCaixa**2)) * (n**2)
+    elif particula == 'Proton':
+        energia = ((PLANCKJS**2) / 8 * MASSAPROTON * (ladoCaixa**2)) * (n**2)
+
+    energiaConvertida = conversor(energia,'Js')
+    return energia, energiaConvertida
+
+def conversor(energia, medida):
+    if medida == 'Ev':
+        energia = energia * 1.602e-19
+    elif medida == 'Js':
+        energia = energia / 1.602e-19
+
+    return energia
+
+def energiaFoton(n):
+    energiaFoton = -13.6 / (n**2)
+    return energiaFoton
+
+def foton():
+    energiaInicial = energiaFoton(nInicial)
+    energiaFinal = energiaFoton(nFinal)
+    energiaAbsoluta = abs(energiaFinal - energiaInicial)
+
+    comprimentoOnda = (PLANCKEV * LUZ) / energiaAbsoluta
+    frequenciaFoton = energiaAbsoluta / PLANCKEV
+
+    return comprimentoOnda,frequenciaFoton
+
+
+def velocidadeParticula(particula):
+    if particula == 'Eletron':
+        energiaInicial = energiaNivelQuantico(ladoCaixa,nInicial,"Eletron")
+        energiaFinal = energiaNivelQuantico(ladoCaixa,nFinal,"Eletron")
+        velocidadeInicial = sqrt((2 * energiaInicial[0]) / MASSAELETRON)
+        velocidadeFinal = sqrt((2 * energiaFinal[0]) / MASSAELETRON)
+    elif particula == 'Proton':
+        energiaInicial = energiaNivelQuantico(ladoCaixa,nInicial,"Proton")
+        energiaFinal = energiaNivelQuantico(ladoCaixa,nFinal,"Proton")
+        velocidadeInicial = sqrt((2 * energiaInicial[0]) / MASSAPROTON)
+        velocidadeFinal = sqrt((2 * energiaFinal[0]) / MASSAPROTON)
+
+    return velocidadeInicial, velocidadeFinal
+
+def comprimentoOndaDeBroglie(particula):
+    if particula == 'Eletron':
+        velocidade = velocidadeParticula("Eletron")
+        comprimentoInicial = PLANCKJS / (MASSAELETRON * velocidade[0])
+        comprimentoFinal = PLANCKJS / (MASSAELETRON * velocidade[1])
+    elif particula == 'Proton':
+        velocidade = velocidadeParticula("Proton")
+        comprimentoInicial = PLANCKJS / (MASSAELETRON * velocidade[0])
+        comprimentoFinal = PLANCKJS / (MASSAELETRON * velocidade[1])
+
+    return comprimentoInicial, comprimentoFinal
+    
+
 
 # 1 / lambda = R(1/nf^2 - 1/ni^2)
 # Energia de um fóton = hf (planck e frequencia)
@@ -20,16 +91,8 @@ PLANCK = 4.136 #E-15 em eV
 # absorção  ef > ei
 # emissão ef < ei
 # função = sqrt(2/Lado) sen((n*Pi)/L * x)
-
-EnergiaFinal = -13.6 / nFinal**2
-EnergiaInicial = -13.6 / nInicial**2
-EnergiaFoton = EnergiaFinal - EnergiaInicial
-comprimentoOndaFoton = ((PLANCK*(10**-15)) * (3*(10**8))) / (abs(EnergiaFoton))
-FrequenciaFoton = EnergiaFoton / (PLANCK*10**-15)
-velocidadeInicial = sqrt(2 * EnergiaInicial) / massaParticula
-velocidadeFinal = sqrt(2 * EnergiaFinal) / massaParticula
-comprimentoDeBroglieI = (PLANCK * (10 **-15)) / massaParticula * velocidadeInicial
-comprimentoDeBroglieF = (PLANCK * (10 **-15)) / massaParticula * velocidadeFinal
-
-
-
+# energia n = ((h^2)/ 8m(L^2)) * n^2  
+# massa eletron = 9.11 * 10e-31
+# h = 6.626 * 10e-34 em J*s 
+# massa proton = 1.67 * 10e-27 
+# integral a - b (função de onda)^2 dx
